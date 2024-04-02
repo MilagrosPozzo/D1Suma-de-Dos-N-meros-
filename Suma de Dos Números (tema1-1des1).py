2cfb6c733f5e484de2c01b5dc6ddcfd1caf8d646
MilagrosPozzo <165667216+MilagrosPozzo@users.noreply.github.com>
2024-04-02 20:37:37 -0300

    Add files via upload

---
+++ Suma de Dos Números (tema1-1des1).py
@@ -0,0 +1,8 @@
+# Definir dos variables con números específicos asignados por ti
+print("digite numero uno")
+num1 = int (input()) 
+print("digite numero dos")
+num2 = int (input()) 
+# Mostrar el resultado de la suma
+print("El resultado de la suma es:", resultado)
+print (num1 + num2)
